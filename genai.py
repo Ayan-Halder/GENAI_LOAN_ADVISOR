0eaf4c72f987b739b2a4131bd77909272396a6d3
ayanhalder194 <ayanhalder194@gmail.com>
2024-09-28 19:08:34 +0530

    Updated to V0.1.0 as more information and details added

--- genai.py
+++ genai.py
@@ -1,6 +1,7 @@
 import google.generativeai as genai
 import os
 
+# Configure the GenAI API
 genai.configure(api_key=os.environ.get("GEMINI_API_KEY"))
 model = genai.GenerativeModel("gemini-1.5-flash")
 
